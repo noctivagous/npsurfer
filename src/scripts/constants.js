77638aef7ed4a5bbd2aaa492dbaa8590ef46376a
noctivagous <119089025+noctivagous@users.noreply.github.com>
2022-11-26 04:46:40 -0600

    first commit.

---
+++ src/scripts/constants.js
@@ -0,0 +1,241 @@
+
+// ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇
+// CONSTANTS.JS
+// ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇
+// --------
+//  --- default preferences obj.
+//  --- 
+// 
+// 
+// --------
+
+
+
+// For event.code:
+let numpadDigitsCodesArray = ["Numpad1", "Numpad2", "Numpad3", "Numpad4", "Numpad5", "Numpad6", "Numpad7", "Numpad8", "Numpad9"];
+
+// For event.code:
+let regularKeysCodesArray = ["KeyJ","KeyK","KeyL","Semicolon",
+                            "KeyU","KeyI","KeyO","KeyP"
+                             ];
+
+
+//  ░░░░░░░░░░░░░░░░░░░░░░░
+//  DEFAULT PREFS
+//  ░░░░░░░░░░░░░░░░░░░░░░░
+
+
+// -------
+// STORED PREFS
+// -------
+
+var storedPrefs = {
+
+    "version": "0.43",
+  
+    "onOffStatus" : "on",
+  
+    "modeForWebsurfer" : "linkBrowser", // "bookmarksBrowser"
+  
+    "browserInputOfWebsurfer" : "numberpad",  // "regularKeys"
+  
+    "prefURLPathnameSelectorModeKey" : "Backslash",
+  
+    "prefURLRootOfDomain" : "Backslash",
+  
+    /* "prefURLPageForANewTab" : "Backslash+Shift", */
+  
+    "prefURLGoogle" : "Backslash+Shift",
+  
+    "prefToggleWebsurferModeNumpad" : "Numpad0",
+  
+    "prefToggleWebsurferModeRegularKeys" : "Digit0",
+  
+    "modifierKeyForLabels" : "alt",
+    
+    /* BACK AND FORWARD KEYS */
+    "prefBackKeyNumpad": "NumpadDivide",
+  
+    "prefForwardKeyNumpad": "NumpadMultiply",
+  
+    "prefBackKeyRegularKeys": "Minus",
+  
+    "prefForwardKeyRegularKeys": "Equal",
+  
+    "prefExtraEscapeKeyNumpad" : "NumpadDecimal",
+  
+    "prefExtraEscapeKeyRegularKeys" : "Period",
+  
+    "prefCloseTabNumpad": "NumpadMultiply",
+  
+    "prefCloseTabRegularKeys": "Backquote",
+  
+    "prefTabTravelLeftKey" : "Minus",
+  
+    "prefTabTravelRightKey" : "Plus",
+  
+    "prefNavigateCurrentURLKey" : "Prime",
+  
+    "prefSearchAndAddressBarKey" : "Backslash",
+  
+    //"employee":{"name":"name", "age":30, "city":"New York"},
+  
+  }
+  
+
+  
+// ░░░░░░░░░░░░░░░░░░░░░░░
+//  KEY CHARS FOR TAGGING LINKS
+//  In the order that anchors are listed (from a query)
+//  in the highlighted cell, labels
+//  are applied from this array.
+//  When a key is pressed and the interactive grid
+//  is visible, the event.key is checked against
+//  this array.
+//  This takes place in the window.onkeydown.
+//  The array is listed out vertically.
+// ░░░░░░░░░░░░░░░░░░░░░░░
+
+let keyCharsArrayNumpad = [
+  'a',
+  's',
+  'd',
+  'f',
+  'w',
+  'e',
+  'r',
+  'q',
+  't',
+  'y',
+  'z',
+  'x',
+  'c',
+  'v',
+  'g',
+  'h',
+  'j',
+  'k',
+  'l',
+  ';',
+  'b',
+  'n',
+  'm',
+  ',',
+  'u',
+  'i',
+  'o',
+  'p',
+
+  '⇧a',
+  '⇧s',
+  '⇧d',
+  '⇧f',
+  '⇧w',
+  '⇧e',
+  '⇧r',
+  '⇧q',
+  '⇧t',
+  '⇧y',
+  '⇧z',
+  '⇧x',
+  '⇧c',
+  '⇧v',
+  '⇧g',
+  '⇧h',
+  '⇧j',
+  '⇧k',
+  '⇧l',
+  '⇧;',
+  '⇧b',
+  '⇧n',
+  '⇧m',
+  '⇧,',
+  '⇧u',
+  '⇧i',
+  '⇧o',
+  '⇧p',
+
+  '⇧1',
+  '⇧2',
+  '⇧3',
+  '⇧4',
+  '⇧5',
+  '⇧6',
+  '⇧7',
+  '⇧8',
+  '⇧9',
+
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  
+];
+
+let keyCharsArrayRegularKeys = [
+  'a',
+  's',
+  'd',
+  'f',
+  'w',
+  'e',
+  'r',
+  'q',
+  't',
+  'y',
+  'z',
+  'x',
+  'c',
+  'v',
+  'g',
+  'h',
+  'b',
+  'n',
+  'm',
+  ',',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+
+  '⇧a',
+  '⇧s',
+  '⇧d',
+  '⇧f',
+  '⇧w',
+  '⇧e',
+  '⇧r',
+  '⇧q',
+  '⇧t',
+  '⇧y',
+  '⇧z',
+  '⇧x',
+  '⇧c',
+  '⇧v',
+  '⇧g',
+  '⇧h',
+  '⇧b',
+  '⇧n',
+  '⇧m',
+  '⇧,',
+  '⇧1',
+  '⇧2',
+  '⇧3',
+  '⇧4',
+  '⇧5',
+  '⇧6',
+  '⇧7',
+  '⇧8',
+  '⇧9',
+  
+];
---
+++ src/scripts/pagequeries.js
@@ -0,0 +1,173 @@
+// ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇
+// PAGEQUERIES.JS
+// ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇
+
+
+function makeQueriesForClickableDOMElemLists(dom)
+{
+ // V1
+ anchorNodesPresentInLoadedDOM1 = Array.from(anchorNodesPresentInLoadedDOM1);
+ anchorNodesPresentInLoadedDOM1 = anchorNodesPresentInLoadedDOM1.concat(Array.from(dom.querySelectorAll("a")));
+
+ // V1
+ //buttonNodesPresentInLoadedDOM1 = Array.from(document.querySelectorAll("button"));
+
+ // v2
+ buttonNodesPresentInLoadedDOM1 = Array.from(buttonNodesPresentInLoadedDOM1);
+ const domIterationResults = getCursorPointerDomArray(true,dom);
+ buttonNodesPresentInLoadedDOM1 = buttonNodesPresentInLoadedDOM1.concat( Array.from( domIterationResults[0] ));
+
+   
+
+/*
+ let shadowRootsArray = Array.from(domIterationResults[1]);
+ if(shadowRootsArray.length){
+   console.log("sr: " +  shadowRootsArray.length);
+  }
+
+
+ for (let i = 0; i < shadowRootsArray.length; i++) 
+ {
+  const domIterationResultsSR = getCursorPointerDomArray(true,shadowRootsArray[i]);
+  buttonNodesPresentInLoadedDOM1 = buttonNodesPresentInLoadedDOM1.concat( Array.from( domIterationResultsSR[0] )); 
+  console.log(domIterationResultsSR);
+ }
+*/ 
+
+
+ // many elements on the page are divs
+ // with role="tab". they are not
+ // button tags or link tags
+ dom.querySelectorAll('[role="tab"]').forEach( function (domElem)
+ {
+   buttonNodesPresentInLoadedDOM1.push(domElem);
+ });
+
+
+ // many elements on the page are divs
+ // with role="button". they are not
+ // button tags.
+ dom.querySelectorAll('[role="button"]').forEach( function (domElem)
+ {
+  if(buttonNodesPresentInLoadedDOM1.includes(domElem) == false) 
+  {
+    buttonNodesPresentInLoadedDOM1.push(domElem);
+  }
+ });
+
+ buttonNodesPresentInLoadedDOM1 = removeDuplicates(buttonNodesPresentInLoadedDOM1);
+
+
+
+
+ // V1
+ inputNodesPresentInLoadedDOM1 = Array.from(inputNodesPresentInLoadedDOM1);
+ inputNodesPresentInLoadedDOM1 = inputNodesPresentInLoadedDOM1.concat( Array.from( dom.querySelectorAll("input") ));
+  
+ 
+
+}
+
+
+// ---------
+// UNUSED
+// ---------
+// ░░░░░░░░░░░░░░░░░░░░░░░
+// Get array of all elem. with cursor style set to 'pointer'
+// ░░░░░░░░░░░░░░░░░░░░░░░
+// modified from https://stackoverflow.com/questions/61124638/select-dom-which-have-css-property-cursorpointer
+
+
+function getCursorPointerDomArray(skipAnchorTags, dom) {
+  
+    var shadowRootArray = [];
+  
+    let ar = [];
+    function iterateDomNode(dom) {
+      let c, noIterate = false;
+      try {
+        c = getComputedStyle(dom);
+  
+        // check for shadow root of 
+        // HTMLElement
+       // if (dom.shadowRoot) {
+      //    shadowRootArray.push(dom.shadowRoot);
+       // }
+  
+       let zI = c.getPropertyValue('z-index');
+       const maxZIndexUsedByNW = 30000004;
+       if (zI) 
+       {
+        if(zI > maxZIndexUsedByNW)
+        {
+                     
+          if(c.style)
+          {
+           c.style.zIndex = 29999999;
+          }
+        }
+       }
+  
+  
+        // skip over anchor tags
+        if ((skipAnchorTags == false) || (dom.nodeName != "A")) 
+        {
+          // console.log(dom.nodeName);
+          if (c.getPropertyValue('cursor') == 'pointer') {
+  
+            ar.push(dom);
+            noIterate = true;
+          }
+  
+        }
+        
+      } catch (e) {
+  
+      }
+  
+      if (dom.hasChildNodes() && noIterate == false) {
+        for (let i = 0; i < dom.childNodes.length; i++) {
+          iterateDomNode(dom.childNodes[i]);
+        }
+      }
+      
+    }
+  
+    iterateDomNode(dom);
+    
+  
+    return [ar,shadowRootArray];
+  }
+  
+
+  
+
+
+  /*
+    //  --------
+    //  shadow root
+    //  --------
+
+
+
+    document.querySelectorAll('*').forEach(function (domElem) 
+    {
+  
+      if (domElem.shadowRoot) 
+      {
+        //console.log(domElem.shadowRoot);
+        domElem.shadowRoot.querySelectorAll('a').forEach(function (domElem2) {
+          anchorNodesPresentInLoadedDOM1.push(domElem2);  
+          //console.log("shadow root a");
+        });
+      }
+  
+    });
+
+
+
+
+
+
+
+  */
